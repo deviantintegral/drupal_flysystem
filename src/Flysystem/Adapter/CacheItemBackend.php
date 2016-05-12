bf35ecba6446f49b54d8a06eff20494545059976
Andrew Berry <deviantintegral@gmail.com>
2016-05-12 15:23:01 -0400

    Docs for cacheitembackend

--- src/Flysystem/Adapter/CacheItemBackend.php
+++ src/Flysystem/Adapter/CacheItemBackend.php
@@ -5,24 +5,41 @@ namespace Drupal\flysystem\Flysystem\Adapter;
 use Drupal\Core\Cache\CacheBackendInterface;
 
 /**
+ * Storage backend for cache items.
  *
+ * This class is separated out from CacheItems so we can easily test loading,
+ * saving, and deleting separately from the logic to reach back to a child
+ * Flysystem adapter.
  */
 class CacheItemBackend implements CacheItemBackendInterface {
 
   /**
+   * The Drupal cache backend to store data in.
+   *
    * @var \Drupal\Core\Cache\CacheBackendInterface
    */
   protected $cacheBackend;
 
   /**
+   * Construct a new CacheItemBackend.
    *
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cacheBackend
+   *   The Drupal cache backend to store items in.
    */
   public function __construct(CacheBackendInterface $cacheBackend) {
     $this->cacheBackend = $cacheBackend;
   }
 
   /**
+   * Load a cache item for a given scheme and path.
    *
+   * @param string $scheme
+   *   The scheme of the item to load.
+   * @param string $path
+   *   The path of the item to load.
+   *
+   * @return \Drupal\flysystem\Flysystem\Adapter\CacheItem
+   *   The cache item, or a new cache item if one isn't in the cache.
    */
   public function load($scheme, $path) {
     $key = $this->getCacheKey($scheme, $path);
@@ -39,7 +56,10 @@ class CacheItemBackend implements CacheItemBackendInterface {
   }
 
   /**
+   * Set a cache item in the backend.
    *
+   * @param \Drupal\flysystem\Flysystem\Adapter\CacheItem $item
+   *   The item to set.
    */
   public function set(CacheItem $item) {
     $key = $this->getCacheKey($item->getScheme(), $item->getPath());
@@ -47,7 +67,10 @@ class CacheItemBackend implements CacheItemBackendInterface {
   }
 
   /**
+   * Delete a cache item from the backend.
    *
+   * @param \Drupal\flysystem\Flysystem\Adapter\CacheItem $item
+   *   The cache item to delete.
    */
   public function delete(CacheItem $item) {
     $this->deleteByKey($item->getScheme(), $item->getPath());
@@ -55,13 +78,23 @@ class CacheItemBackend implements CacheItemBackendInterface {
 
   /**
    * Delete an item by a key, saving having to load an item to delete it.
+   *
+   * @param string $scheme
+   *   The scheme of the item to delete.
+   * @param string $path
+   *   The path of the item to delete.
    */
   public function deleteByKey($scheme, $path) {
     $this->deleteMultiple($scheme, [$path]);
   }
 
   /**
+   * Delete multiple paths for a scheme from the cache.
    *
+   * @param string $scheme
+   *   The scheme of the items to delete.
+   * @param array $paths
+   *   The array of paths to delete.
    */
   public function deleteMultiple($scheme, array $paths) {
     $keys = array();
@@ -72,7 +105,15 @@ class CacheItemBackend implements CacheItemBackendInterface {
   }
 
   /**
+   * Get the cache key for a cache item.
    *
+   * @param string $scheme
+   *   The scheme of the cache item.
+   * @param string $path
+   *   The path of the cache item.
+   *
+   * @return string
+   *   A hashed key suitable for use in a cache.
    */
   public function getCacheKey($scheme, $path) {
     $key = "$scheme://$path";
